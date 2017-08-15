b94a02e8adc010968108180d54503982acabc50c
Ryan Catlin <ryan.catlin@gmail.com>
2017-08-14 23:59:18 -0400

    Add r29er queries

--- server.js
+++ server.js
@@ -2,9 +2,30 @@ var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { graphql, buildSchema } = require('graphql');
 
+const r29ers = {
+	0: {
+		id: 0,
+		name: 'Ryan Catlin',
+		luckyNumber: 3
+	},
+	4: {
+		id: 4,
+		name: 'Steve Tazmin',
+		luckyNumber: 41
+	}
+};
+
 var schema = buildSchema(`
+	type R29er {
+		id: Int!
+		name: String!
+		luckyNumber: Int!
+	}
+
 	type Query {
 		hello: String
+		fetchR29er(id: Int!): R29er
+		R29ers: [R29er]
 	}
 `);
 
@@ -13,6 +34,12 @@ var root = {
 	hello: () => {
 		return 'Hello World!';
 	},
+	fetchR29er: ({ id }) => {
+		return r29ers[id];
+	},
+	R29ers: () => {
+		return Object.values(r29ers);
+	}
 };
 
 
